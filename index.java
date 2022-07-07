ab5ad58dea78323af19c546701a3044e9ce0066b
daguirre0215sv <102762782+daguirre0215sv@users.noreply.github.com>
2022-07-07 17:50:52 -0600

    Update index.java

--- index.java
+++ index.java
@@ -5,7 +5,7 @@ String  nnombre ="Aarón";
 int edad =21;
 String estudiante = "Ingenieria en Sistemas";
 
-System.out.println("Mi nombre es: " +nnombre+" "+ "y tengo" +" "+edad+ " " +"Años"+" "+"Y soy estudiante de la carrera "+" "+estudiante);  
+System.out.println("Mi nombre es: " +nnombre+" "+ ", tengo" +" "+edad+ " " +"Años"+" "+", soy estudiante de la carrera "+" "+estudiante);  
     
 }
 
