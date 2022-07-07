a010445dfaf56cde245121794f80492663b14286
Aaron <daaguirre0215@gmail.com>
2022-07-07 17:47:25 -0600

    Sé modificó el Index nuevamente

--- index.java
+++ index.java
@@ -3,8 +3,9 @@ public class index{
 public static void main(String[] args) {
 String  nnombre ="Aarón";
 int edad =21;
+String estudiante = "Ingenieria en Sistemas";
 
-System.out.println("Mi nombre es: " +nnombre+" "+ "y tengo" +" "+edad+ " " +"Años");  
+System.out.println("Mi nombre es: " +nnombre+" "+ "y tengo" +" "+edad+ " " +"Años"+" "+"Y soy estudiante de la carrera "+" "+estudiante);  
     
 }
 
